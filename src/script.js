f3e5eb37acf28d6bc2f93ac5611ed6b4d8afdac6
stedyyulius <stedyyulius@gmail.com>
2022-10-07 21:52:30 +0700

    grouping

--- src/script.js
+++ src/script.js
@@ -10,10 +10,41 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+);
+
+cube2.position.x = 2;
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+);
+
+cube3.position.x = -2;
+
+group.add(cube3);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -27,7 +58,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 3;
 scene.add(camera)
 
 /**
